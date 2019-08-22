8018daa1683a62f7e7eab9c7a55c9c4565ac45c4
julianalonzo <julianaubreya@gmail.com>
2019-08-22 14:23:34 -0600

    Added component for primary action buttons

---
+++ src/components/PrimaryButton.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { makeStyles } from '@material-ui/styles';
+
+import ButtonBase from '@material-ui/core/ButtonBase';
+
+const useStyles = makeStyles({
+  button: {
+    fontSize: '14px',
+    padding: '8px 24px',
+    borderRadius: '25px',
+    border: '1px solid transparent',
+    backgroundColor: '#3f51b5',
+    color: '#ffffff',
+    textTransform: 'uppercase',
+    letterSpacing: '1.3px',
+    '&:hover': {
+      backgroundColor: '#283593'
+    }
+  }
+});
+
+export default function PrimaryButton({ label }) {
+  const classes = useStyles();
+  return (
+    <ButtonBase disableRipple disableTouchRipple className={classes.button}>
+      {label}
+    </ButtonBase>
+  );
+}
