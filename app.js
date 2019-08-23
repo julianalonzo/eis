16709d453371179585d0818aee3eee2457ae3f9d
julianalonzo <julianaubreya@gmail.com>
2019-08-22 23:21:32 -0600

    Modified file structure in order to accommodate NodeJS backend server

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+app.get('/api/test', (req, res, next) => {
+  res.send('API');
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
+const SERVER_PORT = 8080;
+app.listen(SERVER_PORT, () => {
+  console.log('EIS server running on port ' + SERVER_PORT);
+});
