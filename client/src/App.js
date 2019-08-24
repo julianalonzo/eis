0a818d03aba9659231644c13953d205927f17be4
julianalonzo <julianaubreya@gmail.com>
2019-08-24 15:51:02 -0600

    Added a container with max with lg in App component

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,20 @@ import React from 'react';
 
 import { HashRouter, Route, Switch } from 'react-router-dom';
 
+import Container from '@material-ui/core/Container';
 import NewTemplatePage from './containers/NewTemplatePage';
 import MainPage from './containers/MainPage';
 
 function App() {
   return (
-    <HashRouter>
-      <Switch>
-        <Route path="/new-template" component={NewTemplatePage} />
-        <Route exact path="/" component={MainPage} />
-      </Switch>
-    </HashRouter>
+    <Container maxWidth="lg">
+      <HashRouter>
+        <Switch>
+          <Route path="/new-template" component={NewTemplatePage} />
+          <Route exact path="/" component={MainPage} />
+        </Switch>
+      </HashRouter>
+    </Container>
   );
 }
 
