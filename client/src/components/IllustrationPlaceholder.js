1510c2bf32679d6fe1d36125d098489689add3ba
julianalonzo <julianaubreya@gmail.com>
2019-08-24 13:59:04 -0600

    Added sizes and header text for IllustrationPlaceholder component

--- client/src/components/IllustrationPlaceholder.js
+++ client/src/components/IllustrationPlaceholder.js
@@ -6,16 +6,34 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import DefaultIllustration from '../assets/illustrations/default.svg';
 import PrimaryButton from './PrimaryButton';
+import grey from '@material-ui/core/colors/grey';
 
 import PropTypes from 'prop-types';
 
 const useStyles = makeStyles({
+  headerContainer: {
+    paddingTop: '10vh',
+    paddingLeft: '64px',
+    marginBottom: '36px'
+  },
+  header: {
+    fontSize: '48px',
+    fontWeight: '700'
+  },
+  headerSubText: {
+    fontSize: '16px',
+    color: grey[500],
+    fontWeight: '500'
+  },
+  illustrationContainer: {
+    marginBottom: '24px'
+  },
   centeredWrapper: {
     textAlign: 'center'
   },
-  image: {
-    width: '320px'
-  },
+  image: props => ({
+    width: props.illustrationSize
+  }),
   secondaryText: {
     color: '#9e9e9e'
   },
@@ -29,13 +47,42 @@ export default function IllustrationPlaceholder({
   alt,
   primaryText,
   secondaryText,
-  action
+  action,
+  size,
+  headerText,
+  headerSubText
 }) {
-  const classes = useStyles();
+  let illustrationSize;
+
+  switch (size) {
+    case 'sm':
+      illustrationSize = '300px';
+      break;
+    case 'md':
+      illustrationSize = '400px';
+      break;
+    case 'lg':
+      illustrationSize = '500px';
+      break;
+    default:
+      illustrationSize = '300px';
+  }
+
+  const classes = useStyles({ illustrationSize });
 
   return (
     <Grid container>
-      <Grid item xs={12}>
+      <Grid item xs={12} className={classes.headerContainer}>
+        {headerText ? (
+          <Typography className={classes.header}>{headerText}</Typography>
+        ) : null}
+        {headerSubText ? (
+          <Typography className={classes.headerSubText}>
+            {headerSubText}
+          </Typography>
+        ) : null}
+      </Grid>
+      <Grid item xs={12} className={classes.illustrationContainer}>
         <div className={classes.centeredWrapper}>
           <img
             src={sourceImage ? sourceImage : DefaultIllustration}
@@ -46,10 +93,14 @@ export default function IllustrationPlaceholder({
       </Grid>
       <Grid item xs={12}>
         <div className={classes.centeredWrapper}>
-          <Typography variant="h5">{primaryText}</Typography>
-          <Typography className={classes.secondaryText}>
-            {secondaryText}
-          </Typography>
+          {primaryText ? (
+            <Typography variant="h5">{primaryText}</Typography>
+          ) : null}
+          {secondaryText ? (
+            <Typography className={classes.secondaryText}>
+              {secondaryText}
+            </Typography>
+          ) : null}
           {action ? (
             <div className={classes.actionButtonContainer}>
               <PrimaryButton label={action.label} />
@@ -65,5 +116,8 @@ IllustrationPlaceholder.propTypes = {
   sourceImage: PropTypes.string.isRequired,
   alt: PropTypes.string.isRequired,
   primaryText: PropTypes.string,
-  secondaryText: PropTypes.string
+  secondaryText: PropTypes.string,
+  size: PropTypes.string,
+  headerText: PropTypes.string,
+  headerSubText: PropTypes.string
 };
