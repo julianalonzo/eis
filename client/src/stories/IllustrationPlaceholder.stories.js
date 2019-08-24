89fe262f46a7cd208098a27a89306a2ae099acce
julianalonzo <julianaubreya@gmail.com>
2019-08-24 13:59:35 -0600

    Added stories for sizes of IllustrationPlaceholder

--- client/src/stories/IllustrationPlaceholder.stories.js
+++ client/src/stories/IllustrationPlaceholder.stories.js
@@ -18,4 +18,28 @@ storiesOf('IllustrationPlaceholder', module)
       secondaryText="And I'm a secondarylabel"
       action={{ label: 'Greet' }}
     />
+  ))
+  .add('sm', () => (
+    <IllustrationPlaceholder
+      primaryText="Hi, I'm a primary label"
+      secondaryText="And I'm a secondarylabel"
+      action={{ label: 'Greet' }}
+      size="sm"
+    />
+  ))
+  .add('md', () => (
+    <IllustrationPlaceholder
+      primaryText="Hi, I'm a primary label"
+      secondaryText="And I'm a secondarylabel"
+      action={{ label: 'Greet' }}
+      size="md"
+    />
+  ))
+  .add('lg', () => (
+    <IllustrationPlaceholder
+      primaryText="Hi, I'm a primary label"
+      secondaryText="And I'm a secondarylabel"
+      action={{ label: 'Greet' }}
+      size="lg"
+    />
   ));
