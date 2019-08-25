ea2789b31dbdc39da5124ff3795b5e7adfedcaf0
julianalonzo <julianaubreya@gmail.com>
2019-08-24 21:42:37 -0600

    Deleted attachment form and property form (for redo)

--- client/src/stories/Attachments.stories.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { storiesOf } from '@storybook/react';
-
-import AttachmentsForm from '../components/AttachmentsForm';
-
-storiesOf('AttachmentsForm', module).add('default', () => <AttachmentsForm />);
--- client/src/stories/NewPropertyForm.stories.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { storiesOf } from '@storybook/react';
-
-import NewPropertyForm from '../components/NewPropertyForm';
-
-storiesOf('NewPropertyForm', module).add('default', () => <NewPropertyForm />);
