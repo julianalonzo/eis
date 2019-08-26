9d894882c880c99d9e09f1dc7cc8285b2fd8c5b3
julianalonzo <julianaubreya@gmail.com>
2019-08-25 19:19:36 -0600

    Moved helperFunctions to utilities and created theme object

---
+++ client/src/utilities/theme.js
@@ -0,0 +1,20 @@
+import { grey, indigo } from '@material-ui/core/colors';
+
+const theme = {
+  color: {
+    primary: indigo[500],
+    secondary: grey[500]
+  },
+  fontSize: 16,
+  shadow: {
+    subtle: '0px 3px 15px rgba(0, 0, 0, 0.2)'
+  },
+  fontWeight: {
+    bold: 500,
+    bolder: 700,
+    boldest: 900
+  },
+  space: 12
+};
+
+export default theme;
