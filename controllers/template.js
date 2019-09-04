243d934ff6182315483e7910f683945f89eb836c
julianalonzo <julianaubreya@gmail.com>
2019-09-04 15:08:26 -0600

    Used saveFiles method to handle uploaded thumbnails in createTemplate route

--- controllers/template.js
+++ controllers/template.js
@@ -1,66 +1,58 @@
+const { toArray } = require('../util/helperFunctions');
+const { saveFiles } = require('../controllers/files');
+
 const Template = require('../models/template');
 
-exports.getTemplates = (req, res, next) => {
-  Template.find()
-    .then(templates => {
-      res.status(200).json({ templates: templates });
-    })
-    .catch(err => {
-      console.log(err);
-    });
+exports.getTemplates = async (req, res, next) => {
+  try {
+    const templates = await Template.find();
+    res.status(200).json({ templates: templates });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-exports.createTemplate = (req, res, next) => {
+exports.createTemplate = async (req, res, next) => {
   // @TODO: Add validation
 
-  let thumbnails = [];
-  if (req.files.thumbnails) {
-    thumbnails = req.files.thumbnails.map(thumbnail => {
-      return {
-        originalname: thumbnail.originalname,
-        mimetype: thumbnail.mimetype,
-        filename: thumbnail.filename,
-        path: thumbnail.path
-      };
+  try {
+    // Ensure that an array is passed to the Template's properties field
+    let properties = [];
+    if (req.body.properties) {
+      properties = toArray(req.body.properties);
+      properties = properties.map(property => {
+        return JSON.parse(property);
+      });
+    }
+
+    // Ensure that an array is passed to the Template's thumbnails field
+    let thumbnails = [];
+    if (req.files.thumbnails) {
+      const uploadedThumbnails = await saveFiles(
+        'thumbnail',
+        req.files.thumbnails
+      );
+
+      thumbnails = uploadedThumbnails.map(uploadedThumbnail => {
+        return uploadedThumbnail.id;
+      });
+    }
+
+    // @TODO: Process attachments
+
+    const template = new Template({
+      name: req.body.name,
+      description: req.body.description ? req.body.description : '',
+      item: {
+        ...JSON.parse(req.body.item),
+        thumbnails: thumbnails
+      },
+      properties: properties
     });
+
+    const savedTemplate = await template.save();
+    res.status(201).json({ template: savedTemplate });
+  } catch (err) {
+    console.log(err);
   }
-
-  let properties = [];
-  if (req.body.properties) {
-    properties = req.body.properties.map(property => {
-      return JSON.parse(property);
-    });
-  }
-
-  let attachments = [];
-  if (req.files.attachments) {
-    req.files.attachments.map(attachment => {
-      return {
-        originalname: attachment.originalname,
-        mimetype: attachment.mimetype,
-        filename: attachment.filename,
-        path: attachment.path
-      };
-    });
-  }
-
-  const template = new Template({
-    name: req.body.name,
-    description: req.body.description,
-    item: {
-      ...JSON.parse(req.body.item),
-      thumbnails: thumbnails
-    },
-    properties: properties,
-    attachments: attachments
-  });
-
-  template
-    .save()
-    .then(savedTemplate => {
-      res.status(201).json({ template: savedTemplate });
-    })
-    .catch(err => {
-      console.log(err);
-    });
 };
