5bf68e53c0de242097563e5c85515b46ad8d6eff
julianalonzo <julianaubreya@gmail.com>
2019-09-04 14:30:02 -0600

    Used async/await in item controller

--- controllers/item.js
+++ controllers/item.js
@@ -1,11 +1,11 @@
 const Item = require('../models/item');
 
-exports.getItems = (req, res, next) => {
-  Item.find()
-    .then(items => {
-      res.status(200).json({ items: items });
-    })
-    .catch(error => {
-      console.log('Error', error);
-    });
+exports.getItems = async (req, res, next) => {
+  try {
+    const items = await Item.find();
+
+    res.status(200).json({ items: items });
+  } catch (err) {
+    console.log(err);
+  }
 };
