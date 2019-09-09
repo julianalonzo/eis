bc333dcc813074846f4789e4deb50fd43eff3474
julianalonzo <julianaubreya@gmail.com>
2019-09-09 14:09:04 -0600

    Implemented parseElementsToJSON function in helperFunctions. The function accepts an array of string objects and returns an array of JSON parsed elements.

--- util/helperFunctions.js
+++ util/helperFunctions.js
@@ -2,10 +2,18 @@ exports.toArray = variable => {
   let array = [];
 
   if (!Array.isArray(variable)) {
-    array = array.push(variable);
+    array.push(variable);
   } else {
     array = variable;
   }
 
   return array;
+};
+
+exports.parseElementsToJSON = variable => {
+  const parsedElements = this.toArray(variable).map(element => {
+    return JSON.parse(element);
+  });
+
+  return parsedElements;
 };
