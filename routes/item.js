f10b9ce1a05c73ccb48d21a685e60547ddfc37d2
julianalonzo <julianaubreya@gmail.com>
2019-09-09 14:12:10 -0600

    Added API route for creating items

--- routes/item.js
+++ routes/item.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const upload = require('../util/fileStorage');
+
 const itemController = require('../controllers/item');
 
 router.get('/', itemController.getItems);
+
+const createItemsUpload = upload.fields([
+  { name: 'fileThumbnails' },
+  { name: 'fileAttachments' }
+]);
+router.post('/new', createItemsUpload, itemController.createItems);
 
 module.exports = router;
