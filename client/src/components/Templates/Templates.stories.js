25d88ca1dea65ea36726f6e0952df556598208cf
julianalonzo <julianaubreya@gmail.com>
2019-09-10 21:12:30 -0600

    Moved Templates stories into Templates folder

--- client/src/components/Templates/Templates.stories.js
+++ client/src/components/Templates/Templates.stories.js
@@ -1,26 +1,17 @@
 import React from 'react';
 
+import customTheme from '../../util/theme';
+
 import { storiesOf } from '@storybook/react';
-
 import { muiTheme } from 'storybook-addon-material-ui';
-import customTheme from '../utilities/theme';
 
-import Templates from '../components/Templates';
+import Templates from '.';
 
 export const templates = [
   {
     _id: '123456',
     name: 'Macbook Pro',
     description: 'Template for the brand new Macbook Pro 2019 version',
-    item: {
-      thumbnails: []
-    }
-  },
-  {
-    _id: '78910',
-    name: 'Macbook Pro',
-    description:
-      'Template for the brand new Macbook Pro 2019 version. Template for the brand new Macbook Pro 2019 version',
     item: {
       thumbnails: []
     }
