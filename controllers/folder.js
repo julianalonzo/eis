99af97d8601a8aea0873f08874eaef9a6bca2dfa
julianalonzo <julianaubreya@gmail.com>
2019-09-11 15:56:48 -0600

    Implemented findChildren recursive function and used it in getFolders controller

--- controllers/folder.js
+++ controllers/folder.js
@@ -2,12 +2,42 @@ const Folder = require('../models/folder');
 
 exports.getFolders = async (req, res, next) => {
   try {
-    const folders = await Folder.find();
+    const folders = await Folder.find({ parent: null });
 
-    res.status(200).json({ folders: folders });
+    const foldersWithChildren = [];
+    for (const folder of folders) {
+      const folderWithChildren = {
+        _id: folder.id,
+        name: folder.name,
+        parent: folder.parent,
+        children: (await this.findChildren(folder.id)) || []
+      };
+
+      foldersWithChildren.push(folderWithChildren);
+    }
+
+    res.status(200).json({ folders: foldersWithChildren });
   } catch (err) {
     console.log(err);
   }
+};
+
+exports.findChildren = async folderId => {
+  const children = await Folder.find({ parent: folderId });
+
+  const childFoldersWithChildren = [];
+  for (const childFolder of children) {
+    const childFolderWithChildren = {
+      _id: childFolder.id,
+      name: childFolder.name,
+      parent: childFolder.parent,
+      children: (await this.findChildren(childFolder.id)) || []
+    };
+
+    childFoldersWithChildren.push(childFolderWithChildren);
+  }
+
+  return childFoldersWithChildren;
 };
 
 exports.createFolder = async (req, res, next) => {
