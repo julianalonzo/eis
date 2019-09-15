06f640d7a5fb1008594d366ff1457bc1fe9ac06c
julianalonzo <julianaubreya@gmail.com>
2019-09-14 18:22:17 -0600

    Removed unnecessary themes

--- client/src/util/theme.js
+++ client/src/util/theme.js
@@ -6,15 +6,6 @@ const theme = {
     secondary: {
       main: '#b5a33f'
     }
-  },
-  fontSize: 16,
-  shadow: {
-    subtle: '0px 3px 15px rgba(0, 0, 0, 0.2)'
-  },
-  fontWeight: {
-    bold: 500,
-    bolder: 700,
-    boldest: 900
   }
 };
 
