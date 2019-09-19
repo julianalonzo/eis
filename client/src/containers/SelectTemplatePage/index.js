17e63827d8f92aa1dc48bf775a7fca352ebc2955
Julian Aubrey Mallare Alonzo <julianaubrey@Julians-MacBook-Pro.local>
2019-09-19 14:46:36 -0600

    Removed page heading outside Grid in SelectTemplate page

--- client/src/containers/SelectTemplatePage/index.js
+++ client/src/containers/SelectTemplatePage/index.js
@@ -1,44 +1,44 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 
-import { HOST } from "../../util/constants";
+import { HOST } from '../../util/constants';
 
-import { connect } from "react-redux";
-import * as actions from "../../store/actions";
-import { withRouter } from "react-router-dom";
+import { connect } from 'react-redux';
+import * as actions from '../../store/actions';
+import { withRouter } from 'react-router-dom';
 
-import Card from "../../components/UI/Card";
-import LoadingIndicator from "../../components/UI/LoadingIndicator";
+import Card from '../../components/UI/Card';
+import LoadingIndicator from '../../components/UI/LoadingIndicator';
 
-import { makeStyles } from "@material-ui/styles";
-import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
-import Box from "@material-ui/core/Box";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
+import { makeStyles } from '@material-ui/styles';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles(theme => ({
-  pageHeadingContainer: { marginTop: theme.spacing(2) },
+  pageHeading: { marginBottom: theme.spacing(2) },
   noTemplateCard: {
     padding: theme.spacing(3),
-    height: "120px",
+    height: '120px',
     border: `2px dashed ${theme.palette.primary[400]}`,
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "column",
-    cursor: "pointer",
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    cursor: 'pointer',
     color: theme.palette.primary[400],
-    borderRadius: "5px",
-    "&:hover": {
+    borderRadius: '5px',
+    '&:hover': {
       color: theme.palette.primary[500],
       borderColor: theme.palette.primary[500]
     }
   },
   addIcon: {
     marginBottom: theme.spacing(1),
-    color: "inherit"
+    color: 'inherit'
   },
   noTemplateText: {
-    color: "inherit"
+    color: 'inherit'
   }
 }));
 
@@ -60,55 +60,57 @@ function SelectTemplatePage({
   return (
     <React.Fragment>
       {!fetchingTemplates ? (
-        <Grid container spacing={4}>
-          <Grid item xs={12} className={classes.pageHeadingContainer}>
-            <Typography variant="h5">Choose template</Typography>
-          </Grid>
-          <Grid item xs={12} lg={3} md={4}>
-            <Box
-              className={classes.noTemplateCard}
-              onClick={() => {
-                history.push(`/folders/${folderId}/new-item`);
-              }}
-            >
-              <AddCircleOutlineIcon className={classes.addIcon} />
-              <Typography className={classes.noTemplateText}>
-                No Template
-              </Typography>
-            </Box>
-          </Grid>
-          {templates.map(template => {
-            let thumbnailUrl = null;
-
-            if (template.item.thumbnails.length > 0) {
-              thumbnailUrl = `${HOST}/api/files/${template.item.thumbnails[0].filename}`;
-            }
-
-            return (
-              <Grid
-                className={classes.gridItem}
-                item
-                xs={12}
-                lg={3}
-                md={4}
-                key={template._id}
+        <React.Fragment>
+          <Typography variant="h5" className={classes.pageHeading}>
+            Choose template
+          </Typography>
+          <Grid container spacing={4}>
+            <Grid item xs={12} lg={3} md={4}>
+              <Box
+                className={classes.noTemplateCard}
+                onClick={() => {
+                  history.push(`/folders/${folderId}/new-item`);
+                }}
               >
-                <Card
-                  primaryChip={template.item.category}
-                  title={template.name}
-                  subtitle={template.description}
-                  variant="text-subtitle"
-                  image={thumbnailUrl}
-                  onClick={() => {
-                    history.push(
-                      `/folders/${folderId}/new-item?templateId=${template._id}`
-                    );
-                  }}
-                />
-              </Grid>
-            );
-          })}
-        </Grid>
+                <AddCircleOutlineIcon className={classes.addIcon} />
+                <Typography className={classes.noTemplateText}>
+                  No Template
+                </Typography>
+              </Box>
+            </Grid>
+            {templates.map(template => {
+              let thumbnailUrl = null;
+
+              if (template.item.thumbnails.length > 0) {
+                thumbnailUrl = `${HOST}/api/files/${template.item.thumbnails[0].filename}`;
+              }
+
+              return (
+                <Grid
+                  className={classes.gridItem}
+                  item
+                  xs={12}
+                  lg={3}
+                  md={4}
+                  key={template._id}
+                >
+                  <Card
+                    primaryChip={template.item.category}
+                    title={template.name}
+                    subtitle={template.description}
+                    variant="text-subtitle"
+                    image={thumbnailUrl}
+                    onClick={() => {
+                      history.push(
+                        `/folders/${folderId}/new-item?templateId=${template._id}`
+                      );
+                    }}
+                  />
+                </Grid>
+              );
+            })}
+          </Grid>
+        </React.Fragment>
       ) : (
         <LoadingIndicator />
       )}
