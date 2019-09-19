c5ade307632bc7bc8cf233436772677d429b54d2
Julian Aubrey Mallare Alonzo <julianaubrey@Julians-MacBook-Pro.local>
2019-09-19 13:43:40 -0600

    Removed dropzone in UploadDropzone component

--- client/src/components/UI/UploadDropzone/index.js
+++ client/src/components/UI/UploadDropzone/index.js
@@ -1,62 +1,22 @@
-import React, { useCallback, useMemo } from 'react';
+import React, { useCallback } from 'react';
 
 import Button from '../Button';
-import FileUploadIllustration from '../../../assets/illustrations/file_upload.svg';
 import { useDropzone } from 'react-dropzone';
 
 import { makeStyles } from '@material-ui/styles';
 import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 import Grid from '@material-ui/core/Grid';
-import grey from '@material-ui/core/colors/grey';
-import lightGreen from '@material-ui/core/colors/lightGreen';
-import red from '@material-ui/core/colors/red';
-import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles(theme => ({
-  uploadDropzone: {
-    display: 'block',
-    minHeight: '300px',
-    textAlign: 'center',
-    borderRadius: '10px'
-  },
-  fileUploadIllustration: {
-    width: '150px',
-    marginBottom: theme.spacing(2)
-  },
   cloudUploadIcon: {
     marginRight: theme.spacing(1)
   },
   uploadedFilesContainer: {
-    marginBottom: theme.spacing(4)
+    marginBottom: theme.spacing(2)
   }
 }));
 
-const baseStyle = {
-  outline: 'none',
-  borderRadius: '10px',
-  width: '100%',
-  border: '1px dashed transparent',
-  padding: '16px'
-};
-
-const activeStyle = {
-  border: '1px dashed ' + grey[200]
-};
-
-const acceptStyle = {
-  border: '1px dashed ' + lightGreen[200]
-};
-
-const rejectStyle = {
-  border: '1px dashed ' + red[200]
-};
-
-export default function UploadDropzone({
-  onAddFiles,
-  showPlaceholder,
-  children,
-  label
-}) {
+export default function UploadDropzone({ onAddFiles, children, label }) {
   const classes = useStyles();
 
   const onDrop = useCallback(
@@ -66,59 +26,28 @@ export default function UploadDropzone({
     [onAddFiles]
   );
 
-  const {
-    getRootProps,
-    getInputProps,
-    isDragActive,
-    isDragAccept,
-    isDragReject,
-    open
-  } = useDropzone({ onDrop: onDrop, noClick: true, noKeyboard: true });
-
-  const style = useMemo(
-    () => ({
-      ...baseStyle,
-      ...(isDragActive ? activeStyle : {}),
-      ...(isDragAccept ? acceptStyle : {}),
-      ...(isDragReject ? rejectStyle : {})
-    }),
-    [isDragActive, isDragAccept, isDragReject]
-  );
+  const { getInputProps, open } = useDropzone({
+    onDrop: onDrop,
+    noClick: true,
+    noKeyboard: true
+  });
 
   return (
-    <div {...getRootProps({ style })}>
+    <div>
       <input {...getInputProps()} />
-      {showPlaceholder ? (
-        <div className={classes.uploadDropzone}>
-          <img
-            src={FileUploadIllustration}
-            alt="File Upoad"
-            className={classes.fileUploadIllustration}
-          />
-          <Typography color="textSecondary" style={{ marginBottom: '8px' }}>
-            Drag and drop {label} here
-          </Typography>
-          <Typography color="textSecondary" style={{ marginBottom: '8px' }}>
-            or
-          </Typography>
+      <Grid container>
+        {children ? (
+          <Grid item xs={12} className={classes.uploadedFilesContainer}>
+            {children}
+          </Grid>
+        ) : null}
+        <Grid item xs={12}>
           <Button color="secondary" variant="outlined" onClick={open}>
             <CloudUploadIcon className={classes.cloudUploadIcon} />
             Add {label}
           </Button>
-        </div>
-      ) : (
-        <Grid container>
-          <Grid item xs={12} className={classes.uploadedFilesContainer}>
-            {children}
-          </Grid>
-          <Grid item xs={12}>
-            <Button color="secondary" variant="outlined" onClick={open}>
-              <CloudUploadIcon className={classes.cloudUploadIcon} />
-              Add {label}
-            </Button>
-          </Grid>
         </Grid>
-      )}
+      </Grid>
     </div>
   );
 }
