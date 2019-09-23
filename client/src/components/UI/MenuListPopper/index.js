559c560115974e27d36e0e58a3676d0ec61460ea
Julian Aubrey Mallare Alonzo <julianaubrey@Julians-MacBook-Pro.local>
2019-09-23 00:11:02 -0600

    Added z-index in MenuListPopper so that they won't overlap with other elements

--- client/src/components/UI/MenuListPopper/index.js
+++ client/src/components/UI/MenuListPopper/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import { makeStyles } from '@material-ui/styles';
 import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 import Grow from '@material-ui/core/Grow';
 import Paper from '@material-ui/core/Paper';
 import Popper from '@material-ui/core/Popper';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    zIndex: theme.zIndex.drawer
+  }
+}));
 
 export default function MenuListPopper({
   isOpen,
@@ -11,6 +18,8 @@ export default function MenuListPopper({
   onClose,
   children
 }) {
+  const classes = useStyles();
+
   return (
     <Popper
       open={isOpen}
@@ -18,6 +27,7 @@ export default function MenuListPopper({
       transition
       disablePortal
       placement="bottom-start"
+      className={classes.root}
     >
       {({ TransitionProps, placement }) => (
         <Grow
@@ -27,7 +37,7 @@ export default function MenuListPopper({
               placement === 'bottom' ? 'center bottom' : 'center top'
           }}
         >
-          <Paper>
+          <Paper elevation={3}>
             <ClickAwayListener onClickAway={onClose}>
               {children}
             </ClickAwayListener>
