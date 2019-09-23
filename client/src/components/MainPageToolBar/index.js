90768e44da8e16e6447374e8ac4438b46a97e102
Julian Aubrey Mallare Alonzo <julianaubrey@Julians-MacBook-Pro.local>
2019-09-23 00:10:21 -0600

    Added margin bottom in MainPageToolBar

--- client/src/components/MainPageToolBar/index.js
+++ client/src/components/MainPageToolBar/index.js
@@ -2,13 +2,31 @@ import React from 'react';
 
 import Button from '../UI/Button';
 
+import { makeStyles } from '@material-ui/styles';
 import AddIcon from '@material-ui/icons/Add';
 import Grid from '@material-ui/core/Grid';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    marginBottom: theme.spacing(4)
+  },
+  primaryActionGrid: {
+    display: 'flex',
+    justifyContent: 'flex-end'
+  }
+}));
+
 export default function MainPageToolBar({ onOpenNewButtonMenu }) {
+  const classes = useStyles();
+
   return (
-    <Grid container justify="flex-end">
-      <Grid item md={2}>
+    <Grid container justify="flex-end" spacing={4} className={classes.root}>
+      <Grid
+        item
+        md={2}
+        justify="flex-end"
+        className={classes.primaryActionGrid}
+      >
         <Button
           color="primary"
           variant="contained"
