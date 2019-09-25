ff94dfb07b892fb4221a78bc82643802f9d2aa81
Julian Aubrey Mallare Alonzo <julianaubrey@Julians-MacBook-Pro.local>
2019-09-25 15:16:54 -0600

    Added text and icon variants in Thumbnail component

--- client/src/components/UI/Thumbnail/Thumbnail.stories.js
+++ client/src/components/UI/Thumbnail/Thumbnail.stories.js
@@ -7,13 +7,18 @@ import { muiTheme } from 'storybook-addon-material-ui';
 
 import Thumbnail from '.';
 
+import FolderIcon from '@material-ui/icons/Folder';
+
 export const mockImage =
   'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp13touch-space-select-201807?wid=892&hei=820&&qlt=80&.v=1529520060550';
+export const mockIcon = <FolderIcon fontSize="large" />;
+export const mockText = 'A';
 
 storiesOf('Thumbnail', module)
   .addDecorator(muiTheme([customTheme]))
-  .add('image', () => <Thumbnail src={mockImage} />)
+  .add('image', () => <Thumbnail variant="image" image={mockImage} />)
+  .add('icon', () => <Thumbnail variant="icon" icon={mockIcon} />)
+  .add('letter', () => <Thumbnail variant="text" text={mockText} />)
   .add('imageWithRemove', () => (
-    <Thumbnail src={mockImage} onRemoveThumbnail={() => {}} />
-  ))
-  .add('text', () => <Thumbnail>M</Thumbnail>);
+    <Thumbnail variant="image" image={mockImage} onRemoveThumbnail={() => {}} />
+  ));
--- client/src/components/UI/Thumbnail/index.js
+++ client/src/components/UI/Thumbnail/index.js
@@ -3,20 +3,39 @@ import React from 'react';
 import { makeStyles } from '@material-ui/styles';
 import CloseIcon from '@material-ui/icons/Close';
 import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles(theme => ({
   root: props => ({
     marginRight: theme.spacing(props.marginRight),
     border: `1px solid ${theme.palette.grey[300]}`,
     borderRadius: '8px',
-    padding: theme.spacing(0.5),
     width: theme.spacing(5),
     height: theme.spacing(5),
-    backgroundImage: `url("${props.src}")`,
+    padding: theme.spacing(0.5)
+  }),
+  image: props => ({
+    backgroundImage: `url("${props.image}")`,
     backgroundPosition: 'center',
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover'
   }),
+  icon: props => ({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: theme.palette.primary.main
+  }),
+  text: props => ({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: theme.palette.primary.main
+  }),
+  textContent: {
+    fontSize: theme.typography.fontSize * 1.75,
+    fontWeight: theme.typography.fontWeightMedium
+  },
   removeIcon: {
     position: 'relative',
     right: theme.spacing(-4.5),
@@ -25,18 +44,43 @@ const useStyles = makeStyles(theme => ({
 }));
 
 export default function Thumbnail({
-  alt,
-  src,
-  variant,
+  variant = 'image',
+  image,
+  icon,
+  text,
   marginRight = 0,
-  children,
   onRemoveThumbnail = null,
   onClick
 }) {
-  const classes = useStyles({ marginRight, src, onClick });
+  const classes = useStyles({ marginRight, image, onClick });
+
+  let variantClass;
+  let content;
+  switch (variant) {
+    case 'image':
+      variantClass = classes.image;
+      content = null;
+      break;
+    case 'icon':
+      variantClass = classes.icon;
+      content = icon;
+      break;
+    case 'text':
+      variantClass = classes.text;
+      content = (
+        <Typography color="primary" className={classes.textContent}>
+          {text}
+        </Typography>
+      );
+      break;
+    default:
+      variantClass = classes.image;
+      content = null;
+  }
+
   return (
-    <div className={classes.root}>
-      <div className={classes.thumbnail}></div>
+    <div className={`${classes.root} ${variantClass}`}>
+      {content}
       {onRemoveThumbnail && (
         <div className={classes.removeIcon}>
           <IconButton size="small" onClick={onRemoveThumbnail}>
