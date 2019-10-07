b7ea7b7e4c42c729114ab4a2f15fb81d0311cee0
Julian Aubrey Mallare Alonzo <julianaubrey@Julians-MBP.hitronhub.home>
2019-10-06 18:38:03 -0600

    Added a primary action button in SectionPaper

--- client/src/components/UI/SectionPaper/index.js
+++ client/src/components/UI/SectionPaper/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import { makeStyles } from '@material-ui/styles';
+import Box from '@material-ui/core/Box';
+import IconButton from '@material-ui/core/IconButton';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 
@@ -8,23 +10,42 @@ const useStyles = makeStyles(theme => ({
   root: {
     padding: theme.spacing(2)
   },
+  headerContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'baseline',
+    marginBottom: theme.spacing(3)
+  },
   title: {
-    marginBottom: theme.spacing(2),
-    color: theme.palette.text.hint,
+    color: theme.palette.text.secondary,
     textTransform: 'uppercase',
-    fontWeight: theme.typography.fontWeightMedium,
-    letterSpacing: '1.2px'
+    letterSpacing: '1.5px'
   }
 }));
 
-export default function SectionPaper({ title, children }) {
+export default function SectionPaper({ title, actionButton, children }) {
   const classes = useStyles();
 
   return (
     <Paper className={classes.root}>
-      <Typography variant="body1" className={classes.title}>
-        {title}
-      </Typography>
+      <Box className={classes.headerContainer}>
+        <Box>
+          <Typography variant="body2" className={classes.title}>
+            {title}
+          </Typography>
+        </Box>
+        {actionButton ? (
+          <Box>
+            <IconButton
+              onClick={actionButton.action}
+              color="primary"
+              size="small"
+            >
+              {actionButton.icon}
+            </IconButton>
+          </Box>
+        ) : null}
+      </Box>
       {children}
     </Paper>
   );
