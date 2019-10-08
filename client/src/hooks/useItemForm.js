036eb30a0a4c80d4affa4c5ac2327c46967d641e
Julian Aubrey Mallare Alonzo <julianaubrey@Julians-MBP.hitronhub.home>
2019-10-07 21:20:44 -0600

    Removed attachments and thumbnails in useItemForm hook

--- client/src/hooks/useItemForm.js
+++ client/src/hooks/useItemForm.js
@@ -1,73 +1,14 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 function useItemForm(initialValues) {
-  const [itemForm, setItemForm] = useState({
+  const [itemForm] = useState({
     itemName: initialValues.itemName || '',
     itemCategory: initialValues.itemCategory || '',
     itemCondition: initialValues.itemCondition || '',
-    properties: initialValues.properties || [],
-    thumbnails: initialValues.thumbnails || [],
-    attachments: initialValues.attachments || []
+    properties: initialValues.properties || []
   });
 
-  useEffect(() => {
-    setItemForm({
-      itemName: initialValues.itemName || '',
-      itemCategory: initialValues.itemCategory || '',
-      itemCondition: initialValues.itemCondition || '',
-      properties: initialValues.properties || [],
-      thumbnails: initialValues.thumbnails || [],
-      attachments: initialValues.attachments || []
-    });
-  }, [initialValues]);
-
-  const addThumbnailsHandler = thumbnails => {
-    setItemForm(previousItemForm => {
-      return {
-        ...previousItemForm,
-        thumbnails: previousItemForm.thumbnails.concat(thumbnails)
-      };
-    });
-  };
-
-  const removeThumbnailHandler = thumbnailIndex => {
-    setItemForm(previousItemForm => {
-      return {
-        ...previousItemForm,
-        thumbnails: previousItemForm.thumbnails.filter(
-          (thumbnail, index) => index !== thumbnailIndex
-        )
-      };
-    });
-  };
-
-  const addAttachmentsHandler = attachments => {
-    setItemForm(previousItemForm => {
-      return {
-        ...previousItemForm,
-        attachments: previousItemForm.attachments.concat(attachments)
-      };
-    });
-  };
-
-  const removeAttachmentHandler = attachmentIndex => {
-    setItemForm(previousItemForm => {
-      return {
-        ...previousItemForm,
-        attachments: previousItemForm.attachments.filter(
-          (attachment, index) => index !== attachmentIndex
-        )
-      };
-    });
-  };
-
-  return [
-    itemForm,
-    addThumbnailsHandler,
-    removeThumbnailHandler,
-    addAttachmentsHandler,
-    removeAttachmentHandler
-  ];
+  return itemForm;
 }
 
 export default useItemForm;
