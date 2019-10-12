21fc54eac2cebd8ad7ae2bed70106c4f1a84cdf2
Julian Aubrey Alonzo <julianaubreya@gmail.com>
2019-10-12 15:34:27 -0600

    Implemented desktop app bar nav

--- client/src/components/UI/AppBar/index.js
+++ client/src/components/UI/AppBar/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 
-import { makeStyles } from '@material-ui/styles';
 import { Link } from 'react-router-dom';
-import MuiAppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+
+import { makeStyles } from '@material-ui/styles';
+import {
+  AccountCircle as AccountIcon,
+  Dashboard as DashboardIcon,
+  TurnedIn as TemplatesIcon
+} from '@material-ui/icons/';
+import {
+  Box,
+  IconButton,
+  AppBar as MuiAppBar,
+  Toolbar,
+  Typography
+} from '@material-ui/core/';
 
 const useStyles = makeStyles(theme => ({
+  grow: {
+    flexGrow: 1
+  },
   root: {
     zIndex: theme.zIndex.drawer + 1
   },
@@ -16,21 +29,49 @@ const useStyles = makeStyles(theme => ({
     '&:hover': {
       textDecoration: 'underline'
     }
+  },
+  desktopNav: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  navButtons: {
+    marginRight: theme.spacing(4)
   }
 }));
 
 export default function AppBar() {
   const classes = useStyles();
 
+  const TemplatesLink = React.forwardRef((props, ref) => (
+    <Link innerRef={ref} to="/templates" {...props} />
+  ));
+
   return (
-    <MuiAppBar className={classes.root}>
-      <Toolbar variant="dense">
-        <Typography variant="h6" color="inherit">
-          <Link to="/" className={classes.link}>
-            EIS
-          </Link>
-        </Typography>
-      </Toolbar>
-    </MuiAppBar>
+    <Box className={classes.grow}>
+      <MuiAppBar className={classes.root}>
+        <Toolbar variant="dense">
+          <Typography variant="h6" color="inherit">
+            <Link to="/" className={classes.link}>
+              EIS
+            </Link>
+          </Typography>
+          <Box className={classes.grow}></Box>
+          <Box className={classes.desktopNav}>
+            <Box className={classes.navButtons}>
+              <IconButton color="inherit">
+                <DashboardIcon />
+              </IconButton>
+              <IconButton color="inherit" component={TemplatesLink}>
+                <TemplatesIcon />
+              </IconButton>
+            </Box>
+            <IconButton color="inherit">
+              <AccountIcon />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </MuiAppBar>
+    </Box>
   );
 }
