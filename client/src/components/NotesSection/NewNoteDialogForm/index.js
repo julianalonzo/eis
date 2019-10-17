0736df37243b9ae6f8c1e097eb9d05c74777bfa2
Julian Aubrey Alonzo <julianaubreya@gmail.com>
2019-10-16 21:46:14 -0600

    Used redux actions and reducer for adding a note in NotesSection

---
+++ client/src/components/NotesSection/NewNoteDialogForm/index.js
@@ -0,0 +1,85 @@
+import React, { useEffect } from 'react';
+
+import { isRequired } from '../../../util/validators';
+
+import { Form, Field } from 'react-final-form';
+
+import Button from '../../UI/Button';
+import Dialog from '../../UI/Dialog';
+import DialogActions from '../../UI/Dialog/DialogActions';
+import DialogContent from '../../UI/Dialog/DialogContent';
+import DialogTitle from '../../UI/Dialog/DialogTitle';
+
+import TextField from '@material-ui/core/TextField';
+
+export default function NewNoteDialogForm({
+  isOpen,
+  onClose,
+  onSubmit,
+  submitting
+}) {
+  let newNoteForm;
+
+  useEffect(() => {
+    if (isOpen) {
+      newNoteForm.reset();
+    }
+  }, [isOpen, newNoteForm]);
+
+  const submitHandler = values => {
+    onSubmit(values.content);
+  };
+
+  return (
+    <Form
+      onSubmit={values => {
+        submitHandler(values);
+      }}
+      render={({ handleSubmit, form }) => {
+        newNoteForm = form;
+
+        return (
+          <form onSubmit={handleSubmit}>
+            <Dialog
+              isOpen={isOpen}
+              onClose={onClose}
+              maxWidth="sm"
+              fullWidth={true}
+            >
+              <DialogTitle onClose={onClose}>New Note</DialogTitle>
+              <DialogContent>
+                <Field name="content" validate={isRequired}>
+                  {({ input, meta }) => {
+                    return (
+                      <TextField
+                        variant="outlined"
+                        margin="dense"
+                        fullWidth
+                        multiline
+                        rows={4}
+                        rowsMax={10}
+                        {...input}
+                        error={meta.error && meta.touched}
+                        helperText={
+                          meta.error && meta.touched ? meta.error : null
+                        }
+                      />
+                    );
+                  }}
+                </Field>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={onClose} disabled={submitting}>
+                  Cancel
+                </Button>
+                <Button type="submit" color="primary" disabled={submitting}>
+                  {submitting ? 'Saving...' : 'Save'}
+                </Button>
+              </DialogActions>
+            </Dialog>
+          </form>
+        );
+      }}
+    />
+  );
+}
--- client/src/components/NotesSection/index.js
+++ client/src/components/NotesSection/index.js
@@ -3,34 +3,58 @@ import React from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../store/actions';
 
+import useDialogState from '../../hooks/useDialogState';
+
+import NewNoteDialogForm from './NewNoteDialogForm';
+import Notes from './Notes';
 import SectionPaper from '../UI/SectionPaper';
 
 import { Add as AddIcon } from '@material-ui/icons';
-import Notes from './Notes';
 
-function NotesSection({ itemId, notes, onRemoveNote }) {
+function NotesSection({ itemId, notes, onAddNote, addingNote, onRemoveNote }) {
+  const [
+    isNewNoteDialogOpen,
+    openNewNoteDialogHandler,
+    closeNewNoteDialogHandler
+  ] = useDialogState(false);
+
+  const addNoteHandler = async content => {
+    await onAddNote(itemId, content);
+    closeNewNoteDialogHandler();
+  };
+
   const removeNoteHandler = async noteId => {
     await onRemoveNote(itemId, noteId);
   };
 
   return (
-    <SectionPaper
-      title="Notes"
-      actionButton={{ icon: <AddIcon />, action: () => {} }}
-    >
-      <Notes notes={notes} onRemoveNote={removeNoteHandler} />
-    </SectionPaper>
+    <>
+      <SectionPaper
+        title="Notes"
+        actionButton={{ icon: <AddIcon />, action: openNewNoteDialogHandler }}
+      >
+        <Notes notes={notes} onRemoveNote={removeNoteHandler} />
+      </SectionPaper>
+      <NewNoteDialogForm
+        isOpen={isNewNoteDialogOpen}
+        onClose={closeNewNoteDialogHandler}
+        onSubmit={addNoteHandler}
+        submitting={addingNote}
+      />
+    </>
   );
 }
 
 const mapStateToProps = state => {
   return {
-    removingNote: state.item.removingNote
+    removingNote: state.item.removingNote,
+    addingNote: state.item.addingNote
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
+    onAddNote: (itemId, note) => dispatch(actions.addNote(itemId, note)),
     onRemoveNote: (itemId, noteId) =>
       dispatch(actions.removeNote(itemId, noteId))
   };
