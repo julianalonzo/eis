9ab491776960f400d31191407313425e1ad3f7ff
Julian Aubrey Alonzo <julianaubreya@gmail.com>
2019-10-22 11:57:35 -0600

    Disabled last breadcrumb on Breadcrumbs component

--- client/src/components/UI/Breadcrumbs/index.js
+++ client/src/components/UI/Breadcrumbs/index.js
@@ -2,16 +2,41 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
-import { Breadcrumbs as MuiBreadcrumbs } from '@material-ui/core';
+import { Breadcrumbs as MuiBreadcrumbs, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles(theme => ({
+  link: {
+    color: theme.palette.text.secondary,
+    textDecoration: 'none',
+    '&:hover': {
+      textDecoration: 'underline'
+    }
+  }
+}));
 
 export default function Breadcrumbs({ breadcrumbs }) {
+  const classes = useStyles();
+
   return (
     <MuiBreadcrumbs>
-      {breadcrumbs.map(breadcrumb => {
+      {breadcrumbs.map((breadcrumb, index) => {
+        if (index + 1 < breadcrumbs.length) {
+          return (
+            <Link
+              key={breadcrumb.link}
+              to={breadcrumb.link}
+              className={classes.link}
+            >
+              {breadcrumb.label}
+            </Link>
+          );
+        }
+
         return (
-          <Link key={breadcrumb.link} to={breadcrumb.link}>
+          <Typography key={breadcrumb.link} color="textPrimary">
             {breadcrumb.label}
-          </Link>
+          </Typography>
         );
       })}
     </MuiBreadcrumbs>
